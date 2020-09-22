530987e6700f083eb0878db2a829a5ec3f926906
koalakid1 <koalakid154@gmail.com>
2020-09-22 10:49:28 +0900

    init

---
+++ src/main/java/com/everysports/user/repository/EUserRepository.java
@@ -0,0 +1,8 @@
+package com.everysports.user.repository;
+
+import com.everysports.user.domain.EUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EUserRepository extends JpaRepository<EUser, Long> {
+    EUser findByUserID(Long user_ID);
+}
